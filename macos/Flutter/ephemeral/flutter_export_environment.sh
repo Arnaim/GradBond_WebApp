a7127d2ba86fa2e98c6e1aa8ff5e3321ae5a1c82
Arnaim <cookynaimur@gmailcom>
2025-06-29 01:03:02 +0600

    updated gradle

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=F:\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=G:\Study Folder\7th sem\SDP 4\GradBond_WebApp\GradBond_WebApp"
+export "FLUTTER_APPLICATION_PATH=D:\Android Projects\GradBond_WebApp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
